a416d05771f84ed029ddda08246c08c11581d5d1
andreatripoli <andreatripoli@protonmail.com>
2024-01-19 09:32:31 +0100

    Update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os.path import splitext, basename
 
 setuptools.setup(
     name="mispotx",                     # This is the name of the package
-    version="1.1.4",                        # The initial release version
+    version="1.2.0",                        # The initial release version
     author="Andrea Tripoli",                     # Full name of the author
     description="A tool to push OTXs to MISP",
     long_description="A tool to extract pulses from AlienVault and push them into your MISP instance",
@@ -25,8 +25,8 @@ setuptools.setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'pymisp',
-        'OTXv2'
+        'pymisp>=2.4.183',
+        'OTXv2>=1.5.12'
     ],          # Install other dependencies if any
     license='MIT',                          # License
     url='https://github.com/andreatripoli/mispotx',
